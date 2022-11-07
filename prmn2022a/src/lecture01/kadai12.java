027499121efd7ef62172c7a4709f30fb52e0663a
ppATqq <09tsubasa23@gmail.com>
2022-11-07 19:31:50 +0900

    課題1-2

---
+++ prmn2022a/src/lecture01/kadai12.java
@@ -0,0 +1,11 @@
+package lecture01;
+
+public class kadai12 {
+    public static void main(String[] args){
+
+        int StudentNumber = 2210090;
+
+        System.out.println("B" + StudentNumber + " " + "Tsubasa Arai");
+
+    }
+}
