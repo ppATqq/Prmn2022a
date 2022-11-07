0749d09be0bc52a538a4c25360c521becb3ec45c
ppATqq <09tsubasa23@gmail.com>
2022-11-07 16:05:12 +0900

    課題1-1

---
+++ prmn2022a/src/lecture01/Main.java
@@ -0,0 +1,17 @@
+package lecture01;
+
+public class Main {
+    //psvm+Tabで↓出せる。
+    public static void main(String[] args) {
+
+        System.out.println("We are Project Member.");
+        //soutでショートカット可。
+
+
+        double temp = 18.7;
+        System.out.println("The temperture is" + temp + "degrees");
+
+        int[] array1 = new int[10];
+        array1[4] = 3;
+    }
+}
---
+++ prmn2022a/src/lecture01/kadai11.java
@@ -0,0 +1,10 @@
+package lecture01;
+
+public class kadai11 {
+    public static void main(String[] args) {
+
+        System.out.println("B2210090 Tsubasa Arai");
+
+    }
+}
+
