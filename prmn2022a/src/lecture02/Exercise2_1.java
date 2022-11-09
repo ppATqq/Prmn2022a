691039f15984aaea23118fe32caa02533cabc48a
ppATqq <09tsubasa23@gmail.com>
2022-11-09 10:19:09 +0900

    課題2-1

---
+++ prmn2022a/src/lecture02/Exercise2_1.java
@@ -0,0 +1,14 @@
+package lecture02;
+
+public class Exercise2_1 {
+
+    public static void main(String[] args) {
+
+        Human H1 = new Human("たかし",18);
+        H1.print();
+        Human H2 = new Human("ひろし",20);
+        H2.print();
+
+    }
+
+}
